8b15ab07bda251d868396a7b2d68ce63acc6accb
Pat McCue <psmccue@gmail.com>
2024-03-10 11:45:17 -0600

    reinstalling strapi-admin

--- config/environments/production/database.js
+++ config/environments/production/database.js
@@ -12,9 +12,6 @@ module.exports = ({ env }) => ({
       default: {
         connector: "strapi-hook-bookshelf",
       },
-      // options: {
-      //            ssl: true,
-      //          }
     },
   },
 });
