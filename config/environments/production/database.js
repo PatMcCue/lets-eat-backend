e352b843d673cc83c2684c9da9fb4409d6b9b1eb
Pat McCue <psmccue@gmail.com>
2024-03-10 10:13:21 -0600

    fixing Postgres connection string

--- config/environments/production/database.js
+++ config/environments/production/database.js
@@ -1,25 +1,35 @@
 const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL || "");
 
+console.log(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "strapi-hook-bookshelf",
-      settings: {
-        client: "postgres",
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
-      options: {
-        ssl: true,
-      },
+  postgres: {
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {},
     },
   },
 });
+//   {
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "strapi-hook-bookshelf",
+//       settings: {
+//         client: "postgres",
+//         host: config.host,
+//         port: config.port,
+//         database: config.database,
+//         username: config.user,
+//         password: config.password,
+//         ssl: {
+//           rejectUnauthorized: false,
+//         },
+//       },
+//       options: {
+//         ssl: true,
+//       },
+//     },
+//   },
+// }
