2d67745e7ff69ee6394a7e594eb1678780f8cb75
Pat McCue <psmccue@gmail.com>
2024-03-10 12:09:24 -0600

    Revert "fixing Postgres connection string"

--- config/environments/production/database.js
+++ config/environments/production/database.js
@@ -1,20 +1,29 @@
 const parse = require("pg-connection-string").parse;
 // const config = parse(process.env.DATABASE_URL || "");
 
-console.log(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      schema: "public", // Not required
-      default: {
-        connector: "strapi-hook-bookshelf",
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "strapi-hook-bookshelf",
+      settings: {
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      options: {
+        ssl: true,
       },
     },
   },
 });
+<<<<<<< HEAD
 
 // postgres: {
 //   connection: {
@@ -44,3 +53,5 @@ module.exports = ({ env }) => ({
 //     },
 //   },
 // }
+=======
+>>>>>>> parent of e352b84 (fixing Postgres connection string)
